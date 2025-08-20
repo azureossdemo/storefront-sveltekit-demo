b96019278f962f94bfb6200e509a72b0697b05cb
azureossdemo <ossdemo454242@outlook.com>
2025-08-20 11:12:22 +0000

    Your commit message3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import type { UserConfig } from "vite";
 
 const config: UserConfig = {
 	plugins: [houdini(), sveltekit()],
+	resolve: {
+		alias: {
+			y18n: 'y18n/build/lib/index.js',
+		},
+	},
 	build: {
 		commonjsOptions: {
 			// Enable better interop for CJS packages expecting default exports
